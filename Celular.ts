3813b8ea53defecf707b50fa6fe556d7f30848a8
Uayeb Caballero <uayeb.caballero@gmail.com>
2020-02-11 22:26:15 -0600

    adding new files

---
+++ Celular.ts
@@ -0,0 +1,13 @@
+class Celular{
+
+    id: string;
+    modelo: string;
+
+    constructor(id: string, modelo: string){
+        this.id = id;
+        this.modelo = modelo;
+    }
+
+}
+
+export {Celular}
---
+++ app.ts
@@ -0,0 +1,4 @@
+import {Celular} from "./Celular"
+
+let mi_celular = new Celular("CEL01","DDYUH");
+console.log(mi_celular);
